bbcd24a1489b81ce6860d0b4827a2df88d639806
mle <maximilian@eul.cc>
2018-07-12 18:40:20 +0200

    initial structure

---
+++ test/Helper/AssertException.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace mle86\Enum\Tests\Helper;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\TestCase;
+
+trait AssertException
+{
+
+    /**
+     * Executes the callback (without any arguments)
+     * and expects it to throw an exception whose type is `$exception_class` or a subclass of that.
+     *
+     * @param string|string[] $exception_class  The expected exception FQCN.
+     *                                          Can be an array of FQCNs if multiple exception classes are allowed.
+     * @param callable $callback  The callback to invoke.
+     * @param string $message  The assertion error message.
+     * @return \Throwable  Returns the caught exception.
+     */
+    protected function assertException($exception_class, callable $callback, string $message = ''): \Throwable
+    {
+        $ex = null;
+
+        try {
+            $callback();
+        } catch (\Throwable $ex) {
+            // continue
+        }
+
+        $joined_fqcn = implode('|', (array)$exception_class);
+        $message = "Callback should have thrown a {$joined_fqcn}!" .
+            (($message !== '') ? "\n" . $message : '');
+
+        /** @var TestCase $this */
+        $this->assertNotNull($ex, $message);
+
+        foreach ((array)$exception_class as $fqcn) {
+            if (is_a($ex, $fqcn)) {
+                // ok!
+                return $ex;
+            }
+        }
+
+        throw new AssertionFailedError($message, 0, $ex);
+    }
+
+}
